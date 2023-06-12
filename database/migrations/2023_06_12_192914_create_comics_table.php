40be923848ffb9ec0ebeae930f5f8bf1f8594be1
GiacomoArena <giacomoarena28@gmail.com>
2023-06-12 21:44:14 +0200

    make migration and migrate

--- database/migrations/2023_06_12_192914_create_comics_table.php
+++ database/migrations/2023_06_12_192914_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price', 10);
+            $table->string('series', 100);
+            $table->string('sale_date', 10);
+            $table->string('type', 30);
+            $table->string('artists', 100);
+            $table->string('writers', 100);
             $table->timestamps();
         });
     }
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,6 +6,17 @@
     <div class="container">
         <section class="logo">
             <img src="/img/dc-logo.png" alt="logo">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" >Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" >Elenco Prodotti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" >Contatti</a>
+                </li>
+            </ul>
         </section>
 
 
