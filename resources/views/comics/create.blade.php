40ff09b3ea2256aa14911c827c8473e44257857b
GiacomoArena <giacomoarena28@gmail.com>
2023-06-13 16:11:04 +0200

    create the $fillable

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,25 +1,59 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container">
+    <div class="container col-7">
         <h1>Create a new Comic</h1>
 
         <form action="{{route('comics.store')}}" method="POST">
 
+            @csrf
+
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" >
             </div>
+
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="description" class="form-label">Description</label>
+                <input type="text" class="form-control" id="description" name="description" >
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Thumb</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" >
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control" id="price" name="price" >
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control" id="series" name="series" >
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Sale_date</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date" >
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Type</label>
+                <input type="text" class="form-control" id="type" name="type" >
+            </div>
+
+            <div class="mb-3">
+                <label for="artists" class="form-label">Artists</label>
+                <input type="text" class="form-control" id="artists" name="artists" >
+            </div>
+
+            <div class="mb-3">
+                <label for="writers" class="form-label">Writers</label>
+                <input type="text" class="form-control" id="writers" name="writers" >
+            </div>
+
+            <button type="submit" class="btn btn-success my-5">Submit</button>
         </form>
 
     </div>
